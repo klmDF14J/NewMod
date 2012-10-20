9474ccdb4ff91e26c3c63e548fe464d5f0f2b9c8
Mr KM <km.github@gmail.com>
2012-10-20 23:48:51 +0100

    Initial Commit
    Initial Commit. Contains: 10xColoured Bricks,Handlers,Mud,Biome Gen and
    slime pads

---
+++ BogBiome.java
@@ -0,0 +1,15 @@
+//section 3
+package net.minecraft.src;
+
+import eb99.NewMod;
+
+public class BogBiome extends BiomeGenBase
+{
+	public BogBiome(int par1) {
+		super(par1);
+		this.theBiomeDecorator.setTreesPerChunk(5);
+		//fillerBlock =(byte)Block.glass.blockID;
+		topBlock =(byte)NewMod.mud.blockID; //if u dont want your biome being mod out of your custom block //change blockmod_???.(blockname) to Block.(blockname)
+		setBiomeName("Bogland");
+	}
+}
